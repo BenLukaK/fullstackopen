1fd210ec0917606314d8eee4b09d50bd20e58c29
BenGao <pgaobenp@163.com>
2026-01-04 15:11:14 +0800

    Finished Part 1 - Exercise 1.8 & 1.9

--- part1_d/src/App.jsx
+++ part1_d/src/App.jsx
@@ -7,27 +7,38 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const totalNum = good + neutral + bad;
-  const avgRating = totalNum === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / totalNum;
-  const posPercent = totalNum === 0 ? 0 : good / totalNum;
+  const avgRating = (good * 1 + neutral * 0 + bad * -1) / totalNum;
+  const posPercent = `${(good / totalNum) * 100} %`;
 
 
-  const helper = (stateVar, setStateFunc) => {
-    return () => {
-      setStateFunc(stateVar + 1); 
-    }
-  };
+  const helper = (stateVar, setStateFunc) => () =>
+      setStateFunc(stateVar + 1)
+    
 
   const RateButton = ({stateVar, setStateFunc, buttonText}) => {
     return (
-      <button onClick={helper(stateVar, setStateFunc, buttonText)}>{buttonText}</button>
+      <button onClick={helper(stateVar, setStateFunc)}>{buttonText}</button>
     )
-  };
+  }
 
-  const RateResult = ({stateVar, rateText}) => {
+  const Statistics = ({stateVar, rateText}) => {
     return (
       <div>{rateText} {stateVar}</div>
     )
-  };
+  }
+
+  if (totalNum === 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <RateButton stateVar={good} setStateFunc={setGood} buttonText="good" />
+        <RateButton stateVar={neutral} setStateFunc={setNeutral} buttonText="neutral" />
+        <RateButton stateVar={bad} setStateFunc={setBad} buttonText="bad"/>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -36,14 +47,15 @@ const App = () => {
       <RateButton stateVar={neutral} setStateFunc={setNeutral} buttonText="neutral" />
       <RateButton stateVar={bad} setStateFunc={setBad} buttonText="bad"/>
       <h1>statistics</h1>
-      <RateResult stateVar={good} rateText="good" />
-      <RateResult stateVar={neutral} rateText="neutral" />
-      <RateResult stateVar={bad} rateText="bad" />
-      <RateResult stateVar={totalNum} rateText="all" />
-      <RateResult stateVar={avgRating} rateText="average" />
-      <RateResult stateVar={posPercent} rateText="positive" />
+      <Statistics stateVar={good} rateText="good" />
+      <Statistics stateVar={neutral} rateText="neutral" />
+      <Statistics stateVar={bad} rateText="bad" />
+      <Statistics stateVar={totalNum} rateText="all" />
+      <Statistics stateVar={avgRating} rateText="average" />
+      <Statistics stateVar={posPercent} rateText="positive" />
     </div>
-  );
+  )
+  
 }
 
 export default App
