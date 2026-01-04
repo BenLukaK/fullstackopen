86aa5a94bafb82bdd25cba82b9d042f6c16ba168
BenGao <pgaobenp@163.com>
2026-01-04 15:29:56 +0800

    Finished Part 1 - Exercise 1.10

--- part1_d/src/App.jsx
+++ part1_d/src/App.jsx
@@ -15,47 +15,50 @@ const App = () => {
       setStateFunc(stateVar + 1)
     
 
-  const RateButton = ({stateVar, setStateFunc, buttonText}) => {
+  const RateButton = ({stateVar, setStateFunc, text}) => {
     return (
-      <button onClick={helper(stateVar, setStateFunc)}>{buttonText}</button>
+      <button onClick={helper(stateVar, setStateFunc)}>{text}</button>
     )
   }
 
-  const Statistics = ({stateVar, rateText}) => {
-    return (
-      <div>{rateText} {stateVar}</div>
-    )
-  }
-
-  if (totalNum === 0) {
+  const Statistics = () => {
+    if (totalNum === 0) {
+      return (
+        <div>
+          <h1>statistics</h1>
+          <p>No feedback given</p>
+        </div>
+      )
+    }
     return (
       <div>
-        <h1>give feedback</h1>
-        <RateButton stateVar={good} setStateFunc={setGood} buttonText="good" />
-        <RateButton stateVar={neutral} setStateFunc={setNeutral} buttonText="neutral" />
-        <RateButton stateVar={bad} setStateFunc={setBad} buttonText="bad"/>
         <h1>statistics</h1>
-        <p>No feedback given</p>
+        <StatisticLine value={good} text="good" />
+        <StatisticLine value={neutral} text="neutral" />
+        <StatisticLine value={bad} text="bad" />
+        <StatisticLine value={totalNum} text="all" />
+        <StatisticLine value={avgRating} text="average" />
+        <StatisticLine value={posPercent} text="positive" />
       </div>
+    )
+  }
+
+
+  const StatisticLine = ({value, text}) => {
+    return (
+      <div>{text} {value}</div>
     )
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <RateButton stateVar={good} setStateFunc={setGood} buttonText="good" />
-      <RateButton stateVar={neutral} setStateFunc={setNeutral} buttonText="neutral" />
-      <RateButton stateVar={bad} setStateFunc={setBad} buttonText="bad"/>
-      <h1>statistics</h1>
-      <Statistics stateVar={good} rateText="good" />
-      <Statistics stateVar={neutral} rateText="neutral" />
-      <Statistics stateVar={bad} rateText="bad" />
-      <Statistics stateVar={totalNum} rateText="all" />
-      <Statistics stateVar={avgRating} rateText="average" />
-      <Statistics stateVar={posPercent} rateText="positive" />
+      <RateButton stateVar={good} setStateFunc={setGood} text="good" />
+      <RateButton stateVar={neutral} setStateFunc={setNeutral} text="neutral" />
+      <RateButton stateVar={bad} setStateFunc={setBad} text="bad"/>
+      <Statistics />
     </div>
   )
-  
 }
 
 export default App
