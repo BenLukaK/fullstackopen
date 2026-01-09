94e41e364005b36661e6749ad721ea8fcac8564e
BenGao <pgaobenp@163.com>
2026-01-09 23:30:22 +0800

    Finished Part 2 - Exercise 2.11

--- part2_b/src/App.jsx
+++ part2_b/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filterText, filterHandler}) => {
   return (
@@ -27,7 +28,7 @@ const PersonForm = (props) => {
 const Persons = ({persons}) => {
   return (
     <div>
-      {persons.map(person => <Person key={person.name} person={person} />)}
+      {persons.map(person => <Person key={person.id} person={person} />)}
     </div>
   )
 }
@@ -37,15 +38,19 @@ const Person = ({person}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSelect, setNameSelect] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response)
+        setPersons(response.data)
+      })
+  }, [])
 
   const nameChangeHandler = (e) => {
     setNewName(e.target.value)
